a97e4df5b2ce8b9b0b6dddbc628e2d0083673a19
Jonathas Soares <joul.gs@gmail.com>
2020-12-08 08:32:00 -0300

    [dev] init routes and plans view

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,17 @@
 <?php
 
+use App\Http\Controllers\Admin\PlanController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('/admin/plans', [PlanController::class, 'index'])->name('plans.index'); //dev, futuramente alterar para resource
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+
+/*
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+*/
